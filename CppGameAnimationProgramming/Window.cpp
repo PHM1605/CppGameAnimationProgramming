2b4013a2c89b36841e916b873644441416c2b96d
PHM1605 <PHM1605@gmail.com>
2024-04-26 00:32:09 +0700

    updates

--- CppGameAnimationProgramming/Window.cpp
+++ CppGameAnimationProgramming/Window.cpp
@@ -3,6 +3,54 @@
 
 bool Window::init(unsigned int width, unsigned int height, std::string title) {
 	if (!glfwInit()) {
+		Logger::log(1, "%s: glfwInit() error\n", __FUNCTION__);
 		return false;
 	}
+
+	if (!glfwVulkanSupported()) {
+		glfwTerminate();
+		Logger::log(1, "%s: Vulkan is not supported\n", __FUNCTION__);
+		return false;
+	}
+
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	mApplicationName = title;
+	mWindow = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+	if (!mWindow) {
+		Logger::log(1, "%s: Could not create window\n", __FUNCTION__);
+		glfwTerminate();
+		return false;
+	}
+
+	// glfwMakeContextCurrent(mWindow); // Use OpenGL
+	if (!initVulkan()) {
+		Logger::log(1, "%s: Could not init Vulkan\n", __FUNCTION__);
+		glfwTerminate();
+		return false;
+	}
+
+	Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
+	return true;
+}
+
+bool Window::initVulkan() {
+	return true;
+}
+
+void Window::mainLoop() {
+	glfwSwapInterval(1);
+	float color = 0.0f;
+	while (!glfwWindowShouldClose(mWindow)) {
+		color >= 1.0f ? color = 0.0f : color += 0.01f;
+		glClearColor(color, color, color, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(mWindow);
+		glfwPollEvents();
+	}
+}
+
+void Window::cleanup() {
+	Logger::log(1, "%s: Terminating Window\n", __FUNCTION__);
+	glfwDestroyWindow(mWindow);
+	glfwTerminate();
 }
--- CppGameAnimationProgramming/Window.h
+++ CppGameAnimationProgramming/Window.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
 class Window {
@@ -7,6 +8,11 @@ public:
 	bool init(unsigned int width, unsigned int height, std::string title);
 	void mainLoop();
 	void cleanup();
+
+	bool initVulkan();
 private:
 	GLFWwindow* mWindow = nullptr;
+	std::string mApplicationName;
+	VkInstance mInstance{};
+	VkSurfaceKHR mSurface{};
 };
