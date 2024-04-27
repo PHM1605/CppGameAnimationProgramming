963d65c7a950dedfb36d574b2ebf1de0afc19244
PHM1605 <PHM1605@gmail.com>
2024-04-27 20:57:17 +0700

    updates

--- CppGameAnimationProgramming/Window.cpp
+++ CppGameAnimationProgramming/Window.cpp
@@ -1,8 +1,11 @@
 #include "Window.h"
 #include "Logger.h"
 #include <vector>
+#include <stdexcept>
+#include <iostream>
 
 bool Window::init(unsigned int width, unsigned int height, std::string title) {
+	
 	if (!glfwInit()) {
 		Logger::log(1, "%s: glfwInit() error\n", __FUNCTION__);
 		return false;
@@ -14,6 +17,7 @@ bool Window::init(unsigned int width, unsigned int height, std::string title) {
 		return false;
 	}
 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	mApplicationName = title;
 	mWindow = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
@@ -23,49 +27,73 @@ bool Window::init(unsigned int width, unsigned int height, std::string title) {
 		return false;
 	}
 
+	// Save user pointer
+	glfwSetWindowUserPointer(mWindow, this);
+
+	// ... to use the callbacks
+	glfwSetWindowCloseCallback(mWindow, [](GLFWwindow* win) {
+		auto thisWindow = static_cast<Window*>(glfwGetWindowUserPointer(win));
+		thisWindow->handleWindowCloseEvents();
+		});
+
+	glfwSetKeyCallback(mWindow, [](GLFWwindow* win, int key, int scancode, int action, int mods) {
+		auto thisWindow = static_cast<Window*>(glfwGetWindowUserPointer(win));
+		thisWindow->handleKeyEvents(key, scancode, action, mods);
+		});
+
+	glfwSetMouseButtonCallback(mWindow, [](GLFWwindow* win, int button, int action, int mods) {
+		auto thisWindow = static_cast<Window*>(glfwGetWindowUserPointer(win));
+		thisWindow->handleMouseButtonEvents(button, action, mods);
+		});
+	
 	// glfwMakeContextCurrent(mWindow); // Use OpenGL
 	if (!initVulkan()) {
 		Logger::log(1, "%s: Could not init Vulkan\n", __FUNCTION__);
 		glfwTerminate();
 		return false;
 	}
-
 	Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
 	return true;
 }
 
 bool Window::initVulkan() {
 	VkResult result = VK_ERROR_UNKNOWN;
+	
 	VkApplicationInfo mAppInfo{};
 	mAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	mAppInfo.pNext = nullptr;
-	mAppInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 1, 0);
+	mAppInfo.pApplicationName = "Hello Triangle";
+	mAppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	mAppInfo.pEngineName = "No Engine";
+	mAppInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	mAppInfo.apiVersion = VK_API_VERSION_1_0;
 
+	VkInstanceCreateInfo mCreateInfo{};
+	mCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	mCreateInfo.pApplicationInfo = &mAppInfo;
+
+	// Extensions
 	uint32_t extensionCount = 0;
-	const char** extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
 	if (extensionCount == 0) {
 		Logger::log(1, "%s error: no Vulkan extensions found, need at least 'VK_KHR_SURFACE'\n", __FUNCTION__);
 		return false;
 	}
-
 	Logger::log(1, "%s: Found %u Vulkan extensions\n", __FUNCTION__, extensionCount);
 	for (int i = 0; i < extensionCount; ++i) {
-		Logger::log(1, "%s: %s\n", __FUNCTION__, std::string(extensions[i]).c_str());
+		Logger::log(1, "%s: %s\n", __FUNCTION__, std::string(glfwExtensions[i]).c_str());
 	}
 
-	VkInstanceCreateInfo mCreateInfo{};
-	mCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	mCreateInfo.pNext = nullptr;
-	mCreateInfo.pApplicationInfo = &mAppInfo;
 	mCreateInfo.enabledExtensionCount = extensionCount;
-	mCreateInfo.ppEnabledExtensionNames = extensions;
+	mCreateInfo.ppEnabledExtensionNames = glfwExtensions;
 	mCreateInfo.enabledLayerCount = 0;
+
 	result = vkCreateInstance(&mCreateInfo, nullptr, &mInstance);
 	if (result != VK_SUCCESS) {
 		Logger::log(1, "%s: Could not create Vulkan instance (%i)\n", __FUNCTION__, result);
 		return false;
 	}
-	
+
 	// count physical device
 	uint32_t physicalDeviceCount = 0;
 	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, nullptr);
@@ -73,7 +101,6 @@ bool Window::initVulkan() {
 		Logger::log(1, "%s: No Vulkan capable GPU found\n", __FUNCTION__);
 		return false;
 	}
-
 	// fill with data about GPU
 	std::vector<VkPhysicalDevice> devices;
 	vkEnumeratePhysicalDevices(mInstance, &physicalDeviceCount, devices.data());
@@ -81,11 +108,66 @@ bool Window::initVulkan() {
 	// create surface
 	result = glfwCreateWindowSurface(mInstance, mWindow, nullptr, &mSurface);
 	if (result != VK_SUCCESS) {
-		Logger::log(1, "%s: Could not create Vulkan surface %i\n", __FUNCTION__, result);
+		Logger::log(1, "%s: Could not create Vulkan surface\n", __FUNCTION__);
 		return false;
 	}
-
 	return true;
+}
+
+void Window::handleWindowCloseEvents() {
+	Logger::log(1, "%s: Window got close event...bye!\n", __FUNCTION__);
+}
+
+void Window::handleKeyEvents(int key, int scancode, int action, int mods) {
+	std::string actionName;
+	switch (action) {
+	case GLFW_PRESS:
+		actionName = "pressed";
+		break;
+	case GLFW_RELEASE:
+		actionName = "released";
+		break;
+	case GLFW_REPEAT:
+		actionName = "repeated";
+		break;
+	default:
+		actionName = "invalid";
+		break;
+	}
+	const char* keyName = glfwGetKeyName(key, 0);
+	Logger::log(1, "%s: key %s (key %i, scancode %i) %s\n", __FUNCTION__, keyName, key, scancode, actionName.c_str());
+}
+
+void Window::handleMouseButtonEvents(int button, int action, int mods) {
+	std::string actionName;
+	switch (action) {
+	case GLFW_PRESS:
+		actionName = "pressed";
+		break;
+	case GLFW_RELEASE:
+		actionName = "released";
+		break;
+	default:
+		actionName = "invalid";
+		break;
+	}
+
+	std::string mouseButtonName;
+	switch (button) {
+	case GLFW_MOUSE_BUTTON_LEFT:
+		mouseButtonName = "left";
+		break;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+		mouseButtonName = "middle";
+		break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+		mouseButtonName = "right";
+		break;
+	default:
+		mouseButtonName = "other";
+		break;
+	}
+	Logger::log(1, "%s: %s mouse button (%i) %s\n", __FUNCTION__, mouseButtonName.c_str(), button, actionName.c_str());
 }
 
 void Window::mainLoop() {
@@ -107,3 +189,4 @@ void Window::cleanup() {
 	glfwDestroyWindow(mWindow);
 	glfwTerminate();
 }
+
--- CppGameAnimationProgramming/Window.h
+++ CppGameAnimationProgramming/Window.h
@@ -2,17 +2,25 @@
 #include <string>
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
 
 class Window {
 public:
 	bool init(unsigned int width, unsigned int height, std::string title);
 	void mainLoop();
 	void cleanup();
-
 	bool initVulkan();
 private:
 	GLFWwindow* mWindow = nullptr;
 	std::string mApplicationName;
 	VkInstance mInstance{};
 	VkSurfaceKHR mSurface{};
+
+	void handleWindowCloseEvents();
+	void handleKeyEvents(int key, int scancode, int action, int mods);
+	void handleMouseButtonEvents(int button, int action, int mods);
 };
